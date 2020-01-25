4c81474f29694212eab47f8c29fab1951bafd7de
BlackSwordMan <kirito0930@docomo.ne.jp>
2020-01-25 15:13:59 +0900

    Playerの移動。

---
+++ WAKUWAKU_Shooting/Assets/Scripts/GameView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameView : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        KeyDownCheck();
+    }
+
+    /// <summary>
+    /// どのボタンが押されたかをチェックする
+    /// </summary>
+    void KeyDownCheck()
+    {
+        if(Input.anyKeyDown)
+            foreach (KeyCode code in Enum.GetValues(typeof(KeyCode))) {
+            if (Input.GetKeyDown(code)) {
+                Debug.Log(code);
+                break;
+            }
+        }
+    }
+}
---
+++ WAKUWAKU_Shooting/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField, Range(0.0f, 20.0f)] float _speed;
+    float _moveHorizontal;
+    float _moveVertical;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        _moveHorizontal = Input.GetAxis("Horizontal");
+        _moveVertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(_moveHorizontal * _speed, 0.0f, _moveVertical * _speed);
+    }
+}
