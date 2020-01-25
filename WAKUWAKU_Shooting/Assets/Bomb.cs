d26c45095b13a4b8565fc43fcc4d436f38026039
tsubo <fild.7523@gmail.com>
2020-01-25 17:37:11 +0900

    爆弾追加します

---
+++ WAKUWAKU_Shooting/Assets/Bomb.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _bullet = null;
+
+    private List<GameObject> _list;
+
+    public int _bulletNum;
+
+    public int _hp = 5;
+
+    private float _time=0.0f;
+
+    public void Start()
+    {
+        _list = new List<GameObject>();
+    }
+
+    private void Update()
+    {
+        _time += Time.deltaTime;
+        if (_time>3.0f) { BombAction(); }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            BombAction();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)//ぶつかったら｛｝内の処理が実行
+    {
+        if (other.name != "BombBullet(Clone)") return;
+        Debug.Log("当たった！");
+        _hp--;
+        if (_hp<0) { BombAction(); }
+    }
+
+
+
+
+    private GameObject CreateBullet()
+    {
+        var bullet = GameObject.Instantiate(_bullet);
+        return bullet;
+    }
+
+    private void BombAction() {
+        for (int i = 0; i < _bulletNum; ++i)
+        {
+            _list.Add(CreateBullet());
+        }
+        Destroy(this.gameObject);
+    }
+
+}
---
+++ WAKUWAKU_Shooting/Assets/BombSpawn.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private Bomb _bomb = null;
+
+    public int _maxBombNum = 100;
+    private Bomb[] _bombs = null;
+
+    public int _startBombNum = 5;
+    public float _startSpawnTime = 5.0f;
+    public float _bombSpawnTimeInterval = 0.5f;
+    public float _maxSpawnTime = 2.0f;
+
+    private int _bombsCount = 0;
+
+    void Start()
+    {
+        _bombsCount = _startBombNum;
+        _bombs = new Bomb[_maxBombNum];
+        for (int i = 0; i < _startBombNum; ++i)
+        {
+            CreateBomb();
+        }
+        StartCoroutine(SpawnBomb());
+    }
+
+    void Update()
+    {
+        var a = Time.deltaTime;
+    }
+
+    private void CreateBomb()
+    {
+        _bombs[_bombsCount] = GameObject.Instantiate(_bomb);
+        Vector3 pos;
+        pos.x = Random.Range(0, 10);
+        pos.y = 0;
+        pos.z = Random.Range(0, 10);
+        _bombs[_bombsCount].transform.Translate(pos);
+        _bombsCount++;
+    }
+
+    private IEnumerator CreateBombCoroutine() {
+        CreateBomb();
+        yield return null;//1フレーム
+    }
+    private IEnumerator SpawnBomb()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_startSpawnTime);//秒待つ
+            yield return StartCoroutine(CreateBombCoroutine());
+        }
+    }
+}
